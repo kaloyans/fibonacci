2daf05f156c72941d4acdfd1b178576b25901e9b
Kaloyan Stankov <45176113+kaloyans@users.noreply.github.com>
2018-11-20 14:41:09 +0200

    Create fibonacci.h

---
+++ fibonacci.h
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------------
+
+#ifndef FibonacciH
+#define FibonacciH
+
+class Fibonacci{
+        int firstNumber,
+            secondNumber;
+
+        public:
+        Fibonacci();
+        int Get();
+        int Set(int currentNumber);
+};
+//---------------------------------------------------------------------------
+#endif
